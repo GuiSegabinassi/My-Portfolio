b78cbccb7c294402b64259bffe8f646b28dfb97d
Guilherme Segabinassi Da Cunha <guilhermecunha705@gmail.com>
2025-02-15 22:47:21 -0300

    corrigindo menuBurguer

--- src/js/menuBurguer.js
+++ src/js/menuBurguer.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
+    const menuLinks = menu.querySelectorAll("a");
 
     menuBtn.addEventListener("click", function () {
-        menu.classList.toggle("active");
+        menu.classList.toggle("active"); // Abre/fecha o menu no lado direito
+    });
+
+    // Fechar o menu ao clicar em um link
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            menu.classList.remove("active");
+        });
     });
 });
